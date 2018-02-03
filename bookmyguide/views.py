99c26cf51fb2b94df040324c393a13172a115893
Nitin <nitin.gera.ece14@iitbhu.ac.in>
2018-02-04 01:09:41 +0530

    register DUX profile feature added

--- bookmyguide/views.py
+++ bookmyguide/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from bookmyguide.models import *
 
 def IndexView(request):
     template_name = 'index.html'
@@ -8,4 +11,29 @@ def LoginView(request):
 	pass
 
 def RegisterView(request):
-	pass
+	if request.method == "POST":
+		full_name = request.POST.get('full_name', '')
+		mobile_no = request.POST.get('mobile_no', '')
+		email = 'abc@def.com'
+		password = request.POST.get('password', '')
+
+		try:
+			user = User.objects.create_user(mobile_no, email, password)
+			user.save()
+		except:
+			template_name = 'register.html'
+			return render(request,template_name,{'error':'User already exist'})
+
+		dP = DuxProfile(user=user,full_name=full_name,mobile_number=mobile_no,profile_photo="/static/photo.png")
+		dP.save()
+
+		user = authenticate(username='john', password='secret')
+		if user:
+			if user.is_active:
+				login(request, user)
+		template_name = 'index.html'
+		return render(request,template_name,{})
+		
+	else:
+		template_name = 'register.html'
+		return render(request,template_name,{})
